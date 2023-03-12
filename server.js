2315b5bb750f890cb4946177f9f8853c44678b7c
rodrigo <rodrigo@gocloud.ai>
2023-03-12 15:22:15 -0300

    Builded

--- server.js
+++ server.js
@@ -11,9 +11,16 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
-const server = app.listen(4000, () =>
+const server = app.listen(4000, '0.0.0.0', () =>
   console.log("Server listening on port 4000")
 );
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+  app.get('*', (req, res)=>{
+      res.sendFile(path.join(__dirname + "/client/build/index.html"))
+  })
+}
 
 const io = socket(server, {
   cors: {
